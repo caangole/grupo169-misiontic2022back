1ee58b8796707865628f23f1bbfdfda0bee93621
caangole <caangole@gmail.com>
2020-12-20 20:39:10 -0500

    modificacion main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+axios.defaults.baseURL = 'https://grupo169-misiontic2022back.herokuapp.com/';
 
 new Vue({
   router,
